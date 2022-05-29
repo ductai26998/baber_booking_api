f4cdbc797a7aa68d5d98248f401d3f7d5d7d908d
ductai26998 <ductai26998@gmail.com>
2022-05-29 22:43:58 +0700

    feat[booking]: add booking_service model

--- booking/models.py
+++ booking/models.py
@@ -1,8 +1,7 @@
 from account.models import Salon, User
-from base.models import TimeStampedModel
-from django.db import models
-from base.models import MoneyField
+from base.models import MoneyField, TimeStampedModel
 from django.conf import settings
+from django.db import models
 
 
 class Booking(TimeStampedModel):
@@ -19,3 +18,22 @@ class Booking(TimeStampedModel):
         default=settings.DEFAULT_CURRENCY,
     )
     total_net = MoneyField(amount_field="total_net_amount", currency_field="currency")
+
+
+class BookingService(TimeStampedModel):
+    booking = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="booking_services"
+    )
+    service = models.ForeignKey(
+        Salon, on_delete=models.SET_NULL, related_name="booking_services", null=True
+    )
+    price_amount = models.DecimalField(
+        max_digits=settings.DEFAULT_MAX_DIGITS,
+        decimal_places=settings.DEFAULT_DECIMAL_PLACES,
+        default=0,
+    )
+    currency = models.CharField(
+        max_length=settings.DEFAULT_CURRENCY_CODE_LENGTH,
+        default=settings.DEFAULT_CURRENCY,
+    )
+    price = MoneyField(amount_field="price_amount", currency_field="currency")
