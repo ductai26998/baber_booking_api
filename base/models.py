96a942b33465c6ea68e2a7c66963d2383c815ca8
ductai26998 <ductai26998@gmail.com>
2022-06-15 04:49:52 +0700

    feat[gallery]: upload image to gallery for salon

--- base/models.py
+++ base/models.py
@@ -6,6 +6,10 @@ from django.utils import timezone
 from django_prices.models import MoneyField as BaseMoneyField
 
 
+class MoneyField(BaseMoneyField):
+    serialize = False
+
+
 class TimeStampedModel(models.Model):
     objects = Manager
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -15,5 +19,9 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 
-class MoneyField(BaseMoneyField):
-    serialize = False
+class Photo(TimeStampedModel):
+    url = models.URLField(max_length=512, null=True, blank=True)
+
+    class Meta:
+        abstract = True
+        ordering = ("-created_at",)
---
+++ gallery/__init__.py
@@ -0,0 +1,5 @@
+from base import CoreErrorCode
+
+
+class GalleryErrorCode(CoreErrorCode):
+    pass
