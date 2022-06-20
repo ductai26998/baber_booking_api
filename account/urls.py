eff2b971d8e1409e6b545503bc13f3472deea45d
ductai26998 <ductai26998@gmail.com>
2022-06-20 10:18:25 +0700

    feat[noti]: add fcm_token

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from service import views as service_views
 
+from .views import AccountStoreFCMToken
 from .views import salon as salon_views
 from .views import user as user_views
 
@@ -18,5 +19,6 @@ urlpatterns = [
     path("salonAddress/", salon_views.AddressUpdate.as_view()),
     path("salonBookings/", salon_views.SalonViewSet.as_view({"get": "bookings"})),
     path("userBookings/", user_views.UserViewSet.as_view({"get": "bookings"})),
+    path("fcmToken/", AccountStoreFCMToken.as_view())
     # apis for user
 ]
